e77d2976e9df6e2f485945ae5eedc9ffaf1b343a
Ian Castillo <ian.abraham.castillo@gmail.com>
2024-10-02 10:08:54 -0600

    Initial Commit

---
+++ StaggeredGridViewController/StaggeredGridViewControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaggeredGridViewControllerApp.swift
+//  StaggeredGridViewController
+//
+//  Created by Ian Castillo on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct StaggeredGridViewControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
